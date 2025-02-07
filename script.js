e314dde177bd8ab9542893c77cb667d1c387fd4a
textencoder <160430049+textencoder@users.noreply.github.com>
2025-02-07 15:55:16 -0600

    basic game setup with no restrictions

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-let board = [
-    [null, null, 'o'],
-    [null, 'o', null],
-    ['o', null, null],
-];
+let board = [];
 
-//horizontal wins
-/*for (let i = 0; i < 3; i++) {
-    if (board[i].join('') == 'xxx' || board[i].join('') == 'ooo') {
-        console.log(true);
+const setBoard = function() { 
+        board = [];     
+        for (let i = 0; i < 3; i++) {
+            board.push(Array.of(null, null, null))
+        }
+        return board;
     }
-}*/
 
 function checkWin(board) {
     const size = board.length;
 
     for (let i = 0; i < size; i++) {
-        // Check row
         if (board[i][0] && board[i].every(cell => cell === board[i][0])) {
+            console.log(board[i][0] + ' wins')
             return board[i][0];
+            
         }
-        // Check column
         if (board[0][i] && board.every(row => row[i] === board[0][i])) {
+            console.log(board[0][i] + ' wins')
             return board[0][i];
         }
     }
 
-    // Check diagonals
-    // Top-left to bottom-right
     if (board[0][0] && board.every((row, index) => row[index] === board[0][0])) {
+        console.log(board[0][0] + ' wins')
         return board[0][0];
     }
-    // Top-right to bottom-left
     if (board[0][size - 1] && board.every((row, index) => row[size - 1 - index] === board[0][size - 1])) {
+        console.log(board[0][size - 1] + ' wins')
         return board[0][size - 1];
     }
 
-    // No winner
     return null;
 }
 
-console.log(checkWin(board));
+console.log('set board: ', setBoard())
+
+function markSquare(player, square) {
+    let indexOne = Math.ceil(square / 3);
+    let indexTwo = square > 6 ? square - 6
+    : square > 3 ? square - 3
+    : square; 
+
+    board[indexOne - 1][indexTwo - 1] = player;
+
+    checkWin(board);
+}
