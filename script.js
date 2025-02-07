b28628b6fa5801e88eb3a646731868300f9d10c0
textencoder <160430049+textencoder@users.noreply.github.com>
2025-02-07 16:43:28 -0600

    checks for ties and refuses double inputs

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let board = [];
+let bool = false;
 
 const setBoard = function() { 
         board = [];     
@@ -13,37 +14,47 @@ function checkWin(board) {
 
     for (let i = 0; i < size; i++) {
         if (board[i][0] && board[i].every(cell => cell === board[i][0])) {
-            console.log(board[i][0] + ' wins')
-            return board[i][0];
-            
+            return wrapItUp(board[i][0]);       
         }
         if (board[0][i] && board.every(row => row[i] === board[0][i])) {
-            console.log(board[0][i] + ' wins')
-            return board[0][i];
+            return wrapItUp(board[0][i]);
         }
     }
 
     if (board[0][0] && board.every((row, index) => row[index] === board[0][0])) {
-        console.log(board[0][0] + ' wins')
-        return board[0][0];
+        return wrapItUp(board[0][0]);
     }
     if (board[0][size - 1] && board.every((row, index) => row[size - 1 - index] === board[0][size - 1])) {
-        console.log(board[0][size - 1] + ' wins')
-        return board[0][size - 1];
+        return wrapItUp(board[0][size - 1]);
     }
+
+    if (board[0].every(cell => cell !== null) && board[1].every(cell => cell !== null) && board[2].every(cell => cell !== null)) {
+            return wrapItUp('TIE');       
+        }
+    
 
     return null;
 }
 
-console.log('set board: ', setBoard())
-
-function markSquare(player, square) {
+function markSquare(square) {
     let indexOne = Math.ceil(square / 3);
     let indexTwo = square > 6 ? square - 6
     : square > 3 ? square - 3
     : square; 
 
-    board[indexOne - 1][indexTwo - 1] = player;
-
+    if (board[indexOne - 1][indexTwo - 1] == null) {
+        board[indexOne - 1][indexTwo - 1] = bool ? 'x' : 'o';
+        bool = !bool;
+    } else {
+        throw new Error('space already contains value');
+    }
+    
     checkWin(board);
 }
+
+function wrapItUp(winner) {
+    console.log(winner + ' wins');
+    setBoard();
+}
+
+window.addEventListener('DOMContentLoaded', setBoard);
